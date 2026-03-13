1421c9cb44509abfa63236ffa08591904818de11
LuanaIonta <luanaRionta@gmail.com>
2026-03-12 21:36:11 -0300

    Aula 2  formulário e tabela

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ def home():
 
 @app.route('/revisao')
 def revisao():
-    return '''
-    <h1>Funcionou?</h1>
-    <p>!!!!</p>
-    <p style= "color: purple;">PIP</p>
-    ''' 
+    nome = 'Luana'
+    return render_template('variaveis.html', idade=20, nome=nome)
 
 @app.route('/revisao/<nome>/<int:idade>')
 def parametros(nome, idade):
-    ano_nascimento = 2026 - idade
-    return f'Olá {nome} tenho {idade} anos, por que nasceu no {ano_nascimento} '
+    return render_template('variaveis.html', idade=idade, nome=nome)
+
+@app.route('/produtos/cadastrar')
+def cadastrar_produtos():
+    return render_template('produtos/form_produtos.html')
 
 
 if __name__ == '__main__':
